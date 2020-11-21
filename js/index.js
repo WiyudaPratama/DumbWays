452cec0877560468521b8578f3f304e7f93189f5
WiyudaPratama <wiyudapratama310@gmail.com>
2020-11-21 13:41:58 +0700

    tipe-tipe variable

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
-document.getElementById('hello_world').innerHTML = "My Name is Wiyuda Pratama Mahardika";
+// document.getElementById('hello_world').innerHTML = "My Name is Wiyuda Pratama Mahardika";
 
-console.log("My Name is Wiyuda Pratama Mahardika");
+// console.log("My Name is Wiyuda Pratama Mahardika");
+
+// string
+var myName = "My name is Wiyuda Pratama Mahardika";
+var addreas = 'Medan';
+var my_name = "Dika";
+var MY_NAME = "Pratama";
+
+// integer
+var firstNumber = 1;
+var secondNumber = 5;
+
+// decimal
+var someBody = 1.5;
+
+// boolean
+var isHandsome = true;
+
+// array
+var fruits = ['Apple', "Banana", 'Mellon', 2];
+
+// object
+var car = {
+  name : "Avanza",
+  color : 'black',
+  cc : 3000,
+}
+
+console.log(myName)
+console.log(addreas)
+console.log(my_name)
+console.log(MY_NAME)
+console.log(firstNumber)
+console.log(secondNumber)
+console.log(someBody)
+console.log(isHandsome)
+console.log(fruits)
+console.log(fruits[3])
+console.log(car)
+console.log(car.color)
