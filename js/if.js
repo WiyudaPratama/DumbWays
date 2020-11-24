2f18f7375cfba7a6aa140b1c65ebdace60ee2c81
WiyudaPratama <wiyudapratama310@gmail.com>
2020-11-24 22:10:10 +0700

    if dan else

---
+++ js/if.js
@@ -0,0 +1,17 @@
+// var totalBelanja = prompt("Total belanja anda?", 0) //promt => menampung data dalam inputan
+
+// if(totalBelanja > 3000) {
+//   document.write("<h2>Selamat anda mendapat bonus minuman saset</h2>")
+// }
+
+// document.write("<p>Terimakasi sudah berbelanja ditoko kami</p>")
+
+var password = prompt("Masukkan Password Anda! : ")
+
+if(password == "wiyuda") {
+  document.write("<h2>Selamat anda telah berhasil login</h2>")
+} else{
+  document.write("<h2>Password yang anda inputkan salah</h2>")
+}
+
+document.write("<p>Terimakasih sudah berkunjung ke aplikasi kami</p>")
