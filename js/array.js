b3246773f8a7ee86671c7a38011fb562112772f2
WiyudaPratama <wiyudapratama310@gmail.com>
2020-11-26 14:09:08 +0700

    array

---
+++ js/array.js
@@ -0,0 +1,22 @@
+// Menambahkan array
+// var phone = [];
+
+// phone[0] = "Vivo";
+// phone[1] = "Oppo";
+// phone[2] = "Readmi";
+
+// console.log(phone);
+
+// Mengahpus array
+// var cars = ["Lamborgini", "Tesla", "Alpart"];
+
+// cars[1] = undefined;
+
+// console.log(cars);
+
+// Menampilkan array
+var phones = ["Vivo", "Oppo", "Samsung", "Readmi", "Realme"];
+
+for(var i = 0; i < phones.length; i++) {
+  console.log("Saya mempunyai handphone " + phones[i]);
+}
