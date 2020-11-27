9d585227c547324f5fa2119fcf9c1410e5391bda
WiyudaPratama <wiyudapratama310@gmail.com>
2020-11-27 22:41:49 +0700

    method join, push, dan pop

--- js/array.js
+++ js/array.js
@@ -15,8 +15,27 @@
 // console.log(cars);
 
 // Menampilkan array
-var phones = ["Vivo", "Oppo", "Samsung", "Readmi", "Realme"];
+// var phones = ["Vivo", "Oppo", "Samsung", "Readmi", "Realme"];
 
-for(var i = 0; i < phones.length; i++) {
-  console.log("Saya mempunyai handphone " + phones[i]);
-}
+// for(var i = 0; i < phones.length; i++) {
+//   console.log("Saya mempunyai handphone " + phones[i]);
+// }
+
+// method array
+// join => memisahkan array menggunakan karakter tertentu
+// var phones = ["Vivo", "Oppo", "Samsung", "Readmi", "Realme"];
+// var join = phones.join(" - ");
+
+// console.log(join);
+
+// push => menambahkan array di paling akhir
+// var phones = ["Vivo", "Oppo", "Samsung", "Readmi", "Realme"];
+// phones.push("Advan", "Mito");
+
+// console.log(phones.join(" - "));
+
+// pop => menghapus nilai array yang paling akhir
+var phones = ["Vivo", "Oppo", "Samsung", "Readmi", "Realme"];
+phones.pop();
+
+console.log(phones.join(" - "))
